96a292ba7807e0913a275726466b8e457a964837
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-05-26 15:38:25 +0200

    add password

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 var nome = prompt('dammi il tuo nome');
 // console.log(nome);
+var cognome = prompt('dammi il tuo cognome');
+var colore = prompt('dammi il tuo colore preferito');
+
+var password = nome + cognome + colore + '21';
+alert('ecco la tua Password: ' + password);
