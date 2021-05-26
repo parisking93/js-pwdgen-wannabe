fd004de441b8034a4c48a75d6829bf74b2457429
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-05-26 15:45:55 +0200

    add genera password tutto in minuscolo

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
+// chiedo il nome 
 var nome = prompt('dammi il tuo nome');
-// console.log(nome);
+// chiedo il cognome 
 var cognome = prompt('dammi il tuo cognome');
+// chiedo il colore 
 var colore = prompt('dammi il tuo colore preferito');
-
+// genero password
 var password = nome + cognome + colore + '21';
+password = password.toLowerCase();
+// output 
 alert('ecco la tua Password: ' + password);
